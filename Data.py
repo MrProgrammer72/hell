acaacc748dbe27dda8200fe36bfbe51d6ae5fe86
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-12-14 15:12:33 +0530

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,22 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    generate_single_button = [InlineKeyboardButton("🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")]
+
+    generate_button = [generate_single_button]
+
+    buttons = [
+        generate_single_button,
+        [InlineKeyboardButton("❣️ sᴜᴩᴩᴏʀᴛ ❣️", url="https://t.me/RioGroupsupport"),
+         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/fckualot"),
+        ],
+    ]
+
+    START = """
+Hᴇʏ {},
+Tʜɪs ɪs {},
+Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/rioprojectx/GenerateString)
+Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [Rio](https://t.me/fckualot) !
+    """
